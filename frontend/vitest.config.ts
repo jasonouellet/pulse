f57e6d70c1342334ba8b8460ad06341c2ef1310f
Jason Ouellet <jason.ouellet@cegep-rimouski.qc.ca>
2026-08-31 09:19:19 -0400

    fix: eslint

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,33 +1,30 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
-import sonarReporter from 'vitest-sonar-reporter';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import SonarReporter from "vitest-sonar-reporter";
 
 export default defineConfig({
-    plugins: [react()],
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: './src/test/setup.ts',
-        reporters: [
-            'default',
-            [
-                'vitest-sonar-reporter',
-                {
-                    outputFile: 'test-report-frontend.xml',
-                },
-            ],
-        ],
-        coverage: {
-            provider: 'v8',
-            reporter: ['text', 'lcov', 'clover'],
-            reportsDirectory: './coverage',
-            exclude: [
-                'node_modules/',
-                'src/test/',
-                '**/*.d.ts',
-                '**/*.config.*',
-                '**/snapshot*',
-            ],
-        },
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+    reporters: [
+      "default",
+      new SonarReporter({
+        outputFile: "test-report-frontend.xml",
+      }),
+    ],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "lcov", "clover"],
+      reportsDirectory: "./coverage",
+      exclude: [
+        "node_modules/",
+        "src/test/",
+        "**/*.d.ts",
+        "**/*.config.*",
+        "**/snapshot*",
+      ],
     },
+  },
 });
